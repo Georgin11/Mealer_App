c24324f9b9f8897be8d98b854dfe6a5250bacaaf
macsajder <msajd012uottawa.ca>
2022-10-21 16:50:33 -0400

    Client registration is linked (idk why the street no. won't stay in its text box), the credit card registration is setup, and once you save from the credit card activity it closes registration and returns to "how would you like to use this app?" page.

--- app/src/main/java/com/example/Mealer_App/ClientRegistration.java
+++ app/src/main/java/com/example/Mealer_App/ClientRegistration.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class ClientRegistration extends AppCompatActivity {
+
+    public static boolean payment = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,43 @@ public class ClientRegistration extends AppCompatActivity {
     }
 
     public void onContinue(View view){
-        Intent intent = new Intent(getApplicationContext(), CreditCardInfo.class);
-        startActivity(intent);
+
+        EditText textFirstName = (EditText)findViewById(R.id.editText_ClientName);
+        String firstName = textFirstName.getText().toString();
+
+        EditText textLastName = (EditText)findViewById(R.id.editText_ClientLastName);
+        String lastName = textLastName.getText().toString();
+
+        EditText textEmail = (EditText)findViewById(R.id.editTextEmailAddress);
+        String email = textEmail.getText().toString();
+
+        EditText textPassword = (EditText)findViewById(R.id.editText_ClientPassword);
+        String password = textPassword.getText().toString();
+
+        EditText textStreetNum = (EditText)findViewById(R.id.editText_ClientNumber);
+        String streetNum = textStreetNum.getText().toString();
+
+        EditText textAddress = (EditText)findViewById(R.id.editText_ClientStreet);
+        String street = textAddress.getText().toString();
+
+        EditText textApt = (EditText)findViewById(R.id.editText_ClientApt);
+        String apt = textApt.getText().toString();
+
+        EditText textCity = (EditText)findViewById(R.id.editText_ClientCity);
+        String city = textCity.getText().toString();
+
+        EditText textPostalCode = (EditText)findViewById(R.id.editText_ClientPostal);
+        String postalCode = textPostalCode.getText().toString();
+
+        if(payment == false){
+            Intent intent = new Intent(getApplicationContext(), CreditCardInfo.class);
+            startActivity(intent);
+        }
+        payment = true;
+        finish();
+
     }
+
+
+
 }
--- app/src/main/java/com/example/Mealer_App/CreditCardInfo.java
+++ app/src/main/java/com/example/Mealer_App/CreditCardInfo.java
@@ -2,8 +2,10 @@ package com.example.Mealer_App;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreditCardInfo extends AppCompatActivity {
 
@@ -14,6 +16,30 @@ public class CreditCardInfo extends AppCompatActivity {
     }
 
     public void onSave(View view){
-        finish();
+        EditText textCardholder = (EditText)findViewById(R.id.editTextCardholder);
+        String cardholder = textCardholder.getText().toString();
+
+        EditText textCardNumber = (EditText)findViewById(R.id.editTextCardNumber);
+        String cardNumber = textCardNumber.getText().toString();
+
+        EditText textCVV = (EditText)findViewById(R.id.editTextCVV);
+        String cvv = textCVV.getText().toString();
+
+        EditText textStreetNumber = (EditText)findViewById(R.id.editTextStreetNumber);
+        String streetNum2 = textStreetNumber.getText().toString();
+
+        EditText textStreetAddress = (EditText)findViewById(R.id.editTextBillingAddress);
+        String street2 = textStreetAddress.getText().toString();
+
+        EditText textApt2 = (EditText)findViewById(R.id.editTextBillingApt);
+        String apt2 = textApt2.getText().toString();
+
+        EditText textCity2 = (EditText)findViewById(R.id.editTextCity);
+        String city2 = textCity2.getText().toString();
+
+        EditText textPostal = (EditText)findViewById(R.id.editTextBillingApt);
+        String postal2 = textPostal.getText().toString();
+
+        super.finish();
     }
 }
