87503c2023da29a1420d78c88e1a1e5cb5cfdf57
fthabet1 <thabetfuad@gmail.com>
2022-10-27 18:42:00 -0400

    Implemented basic validators and tested functionality

--- app/src/main/java/com/example/Mealer_App/ClientRegistration.java
+++ app/src/main/java/com/example/Mealer_App/ClientRegistration.java
@@ -8,10 +8,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
+import android.widget.Toast;
 import com.example.Mealer_App.structure.*;
 
+
 public class ClientRegistration extends AppCompatActivity {
+
+    EditText textUsername, textFirstName, textLastName, textEmail, textPassword;
 
     public static boolean payment = false;
     public static Client[] clients = new Client[10];
@@ -20,54 +23,68 @@ public class ClientRegistration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_registration);
+
+        textUsername = findViewById(R.id.editText_ClientUsername);
+        textFirstName = findViewById(R.id.editText_ClientName);
+        textLastName = findViewById(R.id.editText_ClientLastName);
+        textEmail = findViewById(R.id.editTextEmailAddress);
+        textPassword = findViewById(R.id.editText_ClientPassword);
+
+
     }
 
     public void onContinue(View view){
 
-        EditText textUsername = (EditText)findViewById(R.id.editText_ClientUsername);
+        Validators validate = new Validators();
+
+        if(!validate.validateUsername(textUsername) | !validate.validateName(textFirstName) | !validate.validateName(textLastName) | !validate.validateEmail(textEmail) | !validate.validatePassword(textPassword)) {
+            return;
+        }
+
         String username = textUsername.getText().toString();
-
-        EditText textFirstName = (EditText)findViewById(R.id.editText_ClientName);
         String firstName = textFirstName.getText().toString();
-
-        EditText textLastName = (EditText)findViewById(R.id.editText_ClientLastName);
         String lastName = textLastName.getText().toString();
-
-        EditText textEmail = (EditText)findViewById(R.id.editTextEmailAddress);
         String email = textEmail.getText().toString();
-
-        EditText textPassword = (EditText)findViewById(R.id.editText_ClientPassword);
         String password = textPassword.getText().toString();
 
-        EditText textStreetNum = (EditText)findViewById(R.id.editText_ClientNumber);
-        int streetNum = Integer.valueOf(textStreetNum.getText().toString());
-
-        EditText textAddress = (EditText)findViewById(R.id.editText_ClientStreet);
-        String street = textAddress.getText().toString();
-
-        EditText textApt = (EditText)findViewById(R.id.editText_ClientApt);
-        int apt = Integer.valueOf(textApt.getText().toString());
-
-        EditText textCity = (EditText)findViewById(R.id.editText_ClientCity);
-        String city = textCity.getText().toString();
-
-        EditText textPostalCode = (EditText)findViewById(R.id.editText_ClientPostal);
-        String postalCode = textPostalCode.getText().toString();
-
-        if(payment == false){
+        if(!payment){
             Intent intent = new Intent(getApplicationContext(), CreditCardInfo.class);
             startActivity(intent);
         }
-        Address address = new Address(street, streetNum, postalCode, city, apt);
+
+        Address address = null;
+        boolean addressValid = false;
+        while(!addressValid) {
+            try {
+                EditText textAddress = findViewById(R.id.editText_ClientStreet);
+                String street = textAddress.getText().toString();
+
+                EditText textStreetNum = findViewById(R.id.editText_ClientNumber);
+                int streetNum = Integer.parseInt(textStreetNum.getText().toString());
+
+                EditText textPostalCode = findViewById(R.id.editText_ClientPostal);
+                String postalCode = textPostalCode.getText().toString();
+
+                EditText textCity = findViewById(R.id.editText_ClientCity);
+                String city = textCity.getText().toString();
+
+                EditText textApt = findViewById(R.id.editText_ClientApt);
+                int apt = Integer.parseInt(textApt.getText().toString());
+
+                address = new Address(street, streetNum, postalCode, city, apt);
+                addressValid = true;
+            } catch(Exception e) {
+                Toast.makeText(ClientRegistration.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
         clients[numClients++] = new Client(newPaymentInfo, firstName, lastName, email, address, username, password);
 
         payment = true;
         finish();
+
+
         payment = false;
         // allows user to re-enter the loop from signup to registration to credit card again
 
     }
-
-
-
 }
