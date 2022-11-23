857c91433ccc9006d621c4353b0d75b32a3c139a
msajd012 <msajd012@uottawa.ca>
2022-11-23 01:47:08 -0500

    Revert "Made Cook_Meals a new page and formatted it; re-formatted CookLandingPage.java"
    This reverts commit 881b76c460a03545e200031cda374c8f607e3eea.

--- app/src/main/java/com/example/Mealer_App/CookLandingPage.java
+++ app/src/main/java/com/example/Mealer_App/CookLandingPage.java
@@ -25,21 +25,71 @@ import java.util.List;
 
 public class CookLandingPage extends AppCompatActivity {
 
-    // **** CODE MOVED TO CookMeals
-
+    private ListView lv_Menu;
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private TextView textSuspensionLength, textTitle;
+    public static int menuPosition = -1;
+    @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_client_landing_page);
+        setContentView(R.layout.activity_cook_landing_page);
+
+        Database dbHelper = new Database(this);
+        lv_Menu = findViewById(R.id.list_Menu);
+
+        Database db = new Database(this);
+
+        List<Meal> menuList = db.getMealsOfCook(username);
+
+        ArrayAdapter<Complaint> complaintArrayAdapter = new ArrayAdapter(CookLandingPage.this,
+            android.R.layout.simple_list_item_1, menuList);
+
+        lv_Menu.setAdapter(complaintArrayAdapter);
+
+
+        List<Cook> cooks = dbHelper.getCooks();
+        Cook currentCook = null;
+        for(Cook cook : cooks) {
+            if(username.equals(cook.getUsername())) {
+                currentCook = cook;
+            }
+        }
+
+        if(currentCook.getSuspensionLength() != 0) {
+            setContentView(R.layout.activity_cook_suspension);
+
+            String suspensionLength = "";
+            String suspensionTitle = "";
+            if(currentCook.getSuspensionLength() == -5) {
+                suspensionTitle = "You have been permanently banned by our moderation team";
+                suspensionLength = "The only available option for you at the moment is to log out and contact our team for any help you might need";
+                textTitle = findViewById(R.id.text_suspensionTitle);
+                textTitle.setText(suspensionTitle);
+            } else {
+                suspensionLength = "You have been suspended for the next " + currentCook.getSuspensionLength() + " days";
+            }
+
+            textSuspensionLength = findViewById(R.id.text_suspensionLength);
+            textSuspensionLength.setText(suspensionLength);
+
+        }
+
     }
 
     public void GoBack1(View view) {
         finish();
     }
 
-    public void onMealsClick(View view){
-        Intent intent = new Intent(getApplicationContext(), CookMeals.class);
-        startActivity(intent);
+    public void AddMealC(View View) {
+        Intent intent = new Intent(getApplicationContext(), AddMeals.class);
+        startActivityForResult(intent, 0);
     }
+    public void RemoveMealC(View View) {
+        Intent intent = new Intent(getApplicationContext(), RemoveMeals.class);
+        startActivityForResult(intent, 0);
+    }
+
     public void onInstagramClick(View view){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivity(intent);
