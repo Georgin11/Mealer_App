4e1c8efcbe76c4f6014878deffe4195164655184
msajd012 <msajd012@uottawa.ca>
2022-12-02 23:29:15 -0500

    Review tip onClicks updated. Review page should have an intermediary between ClientLandingPage and itself (be able to view prior meal to review).

--- app/src/main/java/com/example/Mealer_App/Review.java
+++ app/src/main/java/com/example/Mealer_App/Review.java
@@ -2,13 +2,41 @@ package com.example.Mealer_App;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Review extends AppCompatActivity {
+
+    int tipAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+    }
+
+    public void onTipClick1(View View) {
+        findViewById(R.id.Tip1).setBackgroundResource(R.drawable.tip_clicked);
+        findViewById(R.id.Tip2).setBackgroundResource(R.drawable.food_back_a);
+        findViewById(R.id.Tip3).setBackgroundResource(R.drawable.food_back_a);
+
+        tipAmount = 1;
+    }
+
+    public void onTipClick2(View View) {
+        findViewById(R.id.Tip1).setBackgroundResource(R.drawable.food_back_a);
+        findViewById(R.id.Tip2).setBackgroundResource(R.drawable.tip_clicked);
+        findViewById(R.id.Tip3).setBackgroundResource(R.drawable.food_back_a);
+
+        tipAmount = 5;
+    }
+
+    public void onTipClick3(View View) {
+        findViewById(R.id.Tip1).setBackgroundResource(R.drawable.food_back_a);
+        findViewById(R.id.Tip2).setBackgroundResource(R.drawable.food_back_a);
+        findViewById(R.id.Tip3).setBackgroundResource(R.drawable.tip_clicked);
+
+        tipAmount = 10;
     }
 }
