1a55349acb02e67b70bbe1f683835a33c6f1236c
msajd012 <msajd012@uottawa.ca>
2022-12-02 20:22:21 -0500

    Review page created

---
+++ app/src/main/java/com/example/Mealer_App/Review.java
@@ -0,0 +1,14 @@
+package com.example.Mealer_App;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+    }
+}
